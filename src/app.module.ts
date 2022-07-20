81c1fd08f57a896d0fcc765a218d278d04ab43e0
N14R56 <2.0turbocoder@gmail.com>
2022-07-19 21:50:50 -0300

    fix: from string to class.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarsModule } from './cars/cars.module';
+import { Car } from './cars/entities/car.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mongodb',
       url: 'mongodb+srv://mmurray:hXqlcMSw8RnGoFgA@digitalgarage.gt4b6a8.mongodb.net/?retryWrites=true&w=majority',
-      entities: ['Car'],
+      entities: [Car],
       synchronize: true,
     }),
     CarsModule,
